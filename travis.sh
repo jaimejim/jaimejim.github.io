048f2125a5c7ff5b7c6658b69986216f451deed9
jaimejim <jaimejim@gmail.com>
2016-08-24 15:24:52 +0300

    edit travis script

--- travis.sh
+++ travis.sh
@@ -3,4 +3,4 @@ set -e # halt script on error
 
 echo 'Testing travis...'
 bundle exec jekyll build
-bundle exec htmlproofer ./_site --disable-external 
+bundle exec htmlproofer ./_site --disable-external --only-4xx
