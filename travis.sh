6f34410523770b95e6998b64564b58e59000f2a9
jaimejim <jaimejim@gmail.com>
2017-11-28 16:58:30 +0200

    jj: updated blog

--- travis.sh
+++ travis.sh
@@ -3,6 +3,6 @@ set -e # halt script on error
 
 #gem install jekyll html-proofer
 
-bundle install
+bundle install --full-index
 bundle exec jekyll build
 bundle exec htmlproofer ./_site --file-ignore /_site/drafts/ --disable-external
