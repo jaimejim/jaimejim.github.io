e71ed53d5918f5617e5d86ae09733c66f5d0e753
Jaime Jimenez <jaimejim@gmail.com>
2020-04-15 19:22:17 +0300

    Added suomen mestari

--- travis.sh
+++ travis.sh
@@ -1,13 +1,30 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-#gem update --system
-gem install 
-gem install mercenary -v 0.3.6
-gem install jekyll html-proofer
-gem install jekyll -v 2.5
 
-bundle install
-bundle update pygments.rb
+
+echo 'Testing travis...'
 bundle exec jekyll build
+bundle exec htmlproofer ./_site --http-status-ignore "403"
+
+
+#gem update --system
+
+
+
+
+# gem install 
+# gem install mercenary -v 0.3.6
+# gem install jekyll html-proofer
+# gem install jekyll -v 2.5
+
+# bundle install
+# bundle update pygments.rb
+# bundle exec jekyll build
+
+
+
+
+
+
 #bundle exec htmlproofer ./_site --file-ignore /_site/drafts/ /_site/slides/ /_site/projects/appiot/ --disable-external
