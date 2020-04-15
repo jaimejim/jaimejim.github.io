1e8e653728b21860b595ef64c6b9318880dd398c
Jaime Jimenez <jaimejim@gmail.com>
2020-04-15 19:30:35 +0300

    Fix travis

--- travis.sh
+++ travis.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
+# gem install
+gem install mercenary -v 0.3.6
 
 
 echo 'Testing travis...'
 bundle exec jekyll build
-bundle exec htmlproofer ./_site --http-status-ignore "403"
+bundle exec htmlproofer ./_site --file-ignore /_site/drafts/ /_site/slides/ /_site/projects/appiot/ --disable-external
 
 
 #gem update --system
 
-
-
-
-# gem install 
 # gem install mercenary -v 0.3.6
 # gem install jekyll html-proofer
 # gem install jekyll -v 2.5
@@ -21,10 +19,3 @@ bundle exec htmlproofer ./_site --http-status-ignore "403"
 # bundle install
 # bundle update pygments.rb
 # bundle exec jekyll build
-
-
-
-
-
-
-#bundle exec htmlproofer ./_site --file-ignore /_site/drafts/ /_site/slides/ /_site/projects/appiot/ --disable-external
